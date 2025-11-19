f75feb9408d963f2add590bb61724dd8f32faf67
Salo <nikolasbott@gmail.com>
2025-11-19 18:05:22 +0100

    added that one random sound on join will be played of multiple random sounds

--- custommods/JingleJoin/src/main/java/de/winterlands/jinglejoin/events/PlayerJoin.java
+++ custommods/JingleJoin/src/main/java/de/winterlands/jinglejoin/events/PlayerJoin.java
@@ -1,6 +1,6 @@
-package de.winterlands.joinjingle.events;
+package de.winterlands.jinglejoin.events;
 
-import de.winterlands.joinjingle.sound.ModSounds;
+import de.winterlands.jinglejoin.sounds.ModSounds;
 import net.minecraft.world.entity.player.Player;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.neoforge.client.event.ClientPlayerNetworkEvent;
@@ -10,8 +10,7 @@ public class PlayerJoin {
     public static void onPlayerJoin(ClientPlayerNetworkEvent.LoggingIn event) {
         Player player = event.getPlayer();
 
-        player.playSound(ModSounds.JOIN_SOUND.value(), 1.0f, 1.0f
-        );
+        player.playSound(ModSounds.getRandomSound().value(), 1.0f, 1.0f);
 
 
     }
