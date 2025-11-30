f93e1d81842f88e2a66bf1aabd0286bc79673b86
Zzackllack <contact@zacklack.de>
2025-11-30 01:47:32 +0100

    chore: Website license override and mod list updates

--- website/src/data/licenseOverrides.ts
+++ website/src/data/licenseOverrides.ts
@@ -30,7 +30,7 @@ export const licenseOverrides: LicenseOverrides = {
     allowByModrinth: false,
     license: "All Rights Reserved",
     sourceUrl:
-      "https://www.curseforge.com/minecraft/mc-mods/christmas-delight-farmers-delight-addon",
+      "https://www.curseforge.com/minecraft/mc-mods/christmas-delight",
   },
   "Christmas Music Discs": {
     note: "Only distributed on CurseForge; author granted direct permission via private message.",
